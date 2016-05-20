23234e995bd6f6351146cecd48e7d4b7a309d301
Arleyn <arleyn.g.2712@hotmail.com>
2016-05-19 20:44:12 -0430

    (AG) Nuevos casos de pruebas, todos dieron proof successfull

---
+++ Theorem3_38.hs
@@ -0,0 +1,19 @@
+module Theroem3_38 where
+import Term
+import Function
+import Theorems
+
+verify = let theorem = ( p /\ p === p ) in
+         proof theorem
+         >>=
+         statement 3.35 with (p =: q) using lambda z (z)
+         >>=
+         statement 3.3 with (p =: p) using lambda z (z <==> p \/ p)
+         >>=
+         statement 3.26 with (p =: p) using lambda z ( true <==> z)
+         >>=
+         statement 3.2 with (true, p =: p, q) using lambda z (z)
+         >>=
+         statement 3.4 with (p =: p) using lambda z (z)
+         >>=
+         done theorem
